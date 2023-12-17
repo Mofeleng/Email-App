cc38db7475de89f90a2373bc8b4465a4ebd29f16
Mofeleng <mofelengmokgope@gmail.com>
2023-12-18 00:52:03 +0200

    🚀Created the form react form

--- src/App.js
+++ src/App.js
@@ -3,22 +3,26 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="app">
+      <div className="container">
+        <h1>Email App</h1>
+        <p>Send an email to anyone</p>
+
+        <form>
+          <label htmlFor="name">Name</label>
+          <input type="text" name='name' id='name' placeholder='enter your name...' />
+
+          <label htmlFor="email">Email</label>
+          <input type="text" name='email' id='email' placeholder='enter the recipient email...' />
+          
+          <label htmlFor="message">Message</label>
+          <textarea name="message" id="message" placeholder='Type your message here'></textarea>
+
+          <button>Send email</button>
+          
+        </form>
+      </div>
+    </section>
   );
 }
 
